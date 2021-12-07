f45f2dfe399d7179d31b349349e48c7653ed16d3
Alex Lewis <alexandroslewis@gmail.com>
2021-12-06 19:03:14 -0800

    committing day 6

---
+++ 6day/2star.ts
@@ -0,0 +1,19 @@
+import obj from './fish';
+const {test, fish} =  obj;
+const days = 256;
+let arr = [...fish];
+
+for (let i = 1; i <= days; i++) {
+    arr = arr.reduce((carry, timer) => {
+        if (timer > 0) {
+            carry.push(timer -1);
+        }
+        if (timer === 0) {
+            carry.push(6);
+            carry.push(8);
+        }
+        return carry;
+    }, []);
+}
+
+console.log(arr.length)
